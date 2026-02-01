328f516e8ec6e389002752c4017c0550b3b84114
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2026-02-01 08:54:26 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//*****************************************************************************
+//** 3010. Divide an Array Into Subarrays With Minimum Cost I       leetcode **
+//*****************************************************************************
+//** Three cuts I place where the smallest leaders stand,                    **
+//** Each subarray crowned by its first command,                             **
+//** I scan for the cheapest trio to align,                                  **
+//** And sum their starting stars to claim the minimum shine.                **
+//*****************************************************************************
+
+int minimumCost(int* nums, int numsSize) {
+    int first = nums[0];
+    int smallest = 100;
+    int second = 100;
+    for(int i = 1; i < numsSize; i++)
+    {
+        if(nums[i] < smallest)
+        {
+            second = smallest;
+            smallest = nums[i];
+        }
+        else if (nums[i] < second)
+        {
+            second = nums[i];
+        }
+    }
+    return first + smallest + second;
+}
